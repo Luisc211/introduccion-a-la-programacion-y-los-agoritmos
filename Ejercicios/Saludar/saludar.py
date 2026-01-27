75a1368a1b280a3f9547c51b7ecb22d751b27b95
Luis Carlos Carrillo <luis.carrillo@octapus.io>
2026-01-27 09:24:49 -0500

    Ejercicio: Saludar

--- Ejercicios/Saludar/saludar.py
+++
@@ -1,4 +0,0 @@
-#Crear un programa en Python que pida al usuario ingresar su nombre, e imprima por pantalla un saludo de 
-#cortesia con el nombre del usuario
-nombre = input ("Por favor ingresa tu nombre ")
-print(f"¡Hola!, {nombre}. Espero estes muy bien")
