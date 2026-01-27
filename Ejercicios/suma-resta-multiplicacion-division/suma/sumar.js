f4fcd86061841ec650ff984725e7378912e57ee9
Luis Carlos Carrillo <luis.carrillo@octapus.io>
2026-01-27 09:28:50 -0500

    Remove files

--- Ejercicios/suma-resta-multiplicacion-division/suma/sumar.js
+++
@@ -1,6 +0,0 @@
-/*Crear un algoritmo que pida al usuario ingresar dos números por teclado, los sume, e imprima 
-el resultado por pantalla.*/
-let num1 = parseFloat(prompt("Ingresa el primer número"))
-let num2 = parseFloat(prompt("Ingresa el segundo número"))
-let resultado = num1+num2
-alert("El resultado de la suma es: " + resultado)
--- Ejercicios/suma-resta-multiplicacion-division/suma/sumar.py
+++
@@ -1,6 +0,0 @@
-#Crear un algoritmo que pida al usuario ingresar dos números por teclado, los sume, e imprima 
-#el resultado por pantalla.
-num1 = float(input ("Ingresa el primer número "))
-num2 = float(input ("Ingresa el segundo número "))
-resultado = num1+num2
-print(f"El resultado de la suma es: {resultado}")
